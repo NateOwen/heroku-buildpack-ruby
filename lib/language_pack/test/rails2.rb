0775107004dba67c8ada53a9f9d55018f10fdc70
Nathan Owen <nowen@a10networks.com>
2019-12-11 14:52:36 -0800

    logging test

--- lib/language_pack/test/rails2.rb
+++ lib/language_pack/test/rails2.rb
@@ -54,7 +54,7 @@ FILE
   private
   def db_prepare_test_rake_tasks
     schema_load    = rake.task("db:schema:load")
-    structure_load = rake.task("db:structure:load_if_sql")
+    structure_load = rake.task("db:structure:load")
     db_migrate     = rake.task("db:migrate")
 
     return [] if db_migrate.not_defined?
